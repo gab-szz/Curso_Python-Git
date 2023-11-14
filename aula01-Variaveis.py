86c4324cd80dbeddc8614de5be44a5aead98ccbe
unknown <silvio.apad@gmail.com>
2023-11-14 17:04:17 -0400

    Carregamento inicial

---
+++ aula01-Variaveis.py
@@ -0,0 +1,15 @@
+#Definindo o tipo de variável
+varString = str(5)
+varInt = int(5)
+varFloat = float(5)
+
+print("Tipos das variáveis: {} {} {}".format(type(varString), type(varInt), type(varFloat)))
+
+#Definindo valores simultaneamente
+
+a, b, c = "A", "B", "C"
+
+a = b = c = "Mesmo valor"
+
+tupla = ["A", "B", "C"]
+
