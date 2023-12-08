5deb0c259b8aa31e209c239c40fd2786ef558d8b
Gab Dev <125615352+gab-szz@users.noreply.github.com>
2023-12-08 18:04:52 -0400

    Foi adicionado os titulos das aulas

--- aula02-Operadores.py
+++ aula02-Operadores.py
@@ -1,1 +1,13 @@
 # Anotações importantes acerca dos operadores
+
+# Operadores Aritiméticos
+
+# Operadores de Atribuição
+
+# Operações de Comparação
+
+# Operadores Lógicos
+
+# Operadores de Identidade
+
+# Operadores de Associação
