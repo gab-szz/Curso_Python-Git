79927cfda9f244e157cd061ff7025dc03154e060
unknown <silvio.apad@gmail.com>
2023-12-08 20:45:48 -0400

    Iniciando as aulas com listas - como criar listas

---
+++ Aula03-Listas.py
@@ -0,0 +1,12 @@
+# Listas em Python
+
+lista = ["um", "dois", "tres", "um"]
+
+print(lista)
+print(len(lista)) # Imprime a quantidade de itens na lista
+
+print(lista[0]) # Acessar um item da lista
+print(lista[1:]) # Mesma regra dos strings
+
+if "dois" in lista:
+    print("\"Dois\" está na lista")
