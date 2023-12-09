390bfb5be2e71989af82520fb231f8deb32d4069
unknown <silvio.apad@gmail.com>
2023-12-08 20:12:34 -0400

    Adicionado operadores aritméticos

--- aula02-Operadores.py
+++ aula02-Operadores.py
@@ -2,6 +2,34 @@
 
 # Operadores Aritiméticos
 
+x = 10
+y = 2
+
+print(x + y) # Soma
+print(x - y) # Subtração
+print(x * y) # Multiplicação
+print(x / y) # Divisão
+
+x = 5
+y = 2
+print(x % y) # Resto da divisão
+
+x = 2
+y = 5
+print(x ** y)# O mesmo que 2*2*2*2*2
+
+x = 15
+y = 2
+
+print(x / y)
+print(x // y) # Floor division
+
+a = 5 + 2 * 4
+print(a)
+
+b = (5 + 2) * 4
+print(b)
+
 # Operadores de Atribuição
 
 # Operações de Comparação
