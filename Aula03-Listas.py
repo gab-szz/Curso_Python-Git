932be617cbda77bc35b447f59501e91f3b557bc0
unknown <silvio.apad@gmail.com>
2023-12-11 20:20:15 -0400

    Aprendendo mais funções sobre as listas.

--- Aula03-Listas.py
+++ Aula03-Listas.py
@@ -10,3 +10,32 @@ print(lista[1:]) # Mesma regra dos strings
 
 if "dois" in lista:
     print("\"Dois\" está na lista")
+
+lista[3] = "cinco"
+lista.insert(3, "quatro") # Substitui o item 3, empurrando-o pra frente na lista
+lista.append("seis") # Acrescenta um item ao fim da lista
+tupla = ('sete', 'oito', 'nove')
+lista.extend(tupla) # Acrescentamos mais objetos
+
+# Removendo itens
+lista.remove("nove") # Remove item pelo conteúdo
+lista.pop(7) # Remove item pelo índice (sem argumento remove o último)
+del lista[6] # Remove pelo índice também
+# del lista     - Apaga a lista inteira
+
+print(lista)
+
+# Imprime os itens da lista
+for item in lista:
+    print(item)
+
+# Imprime os índices da lista
+for indice in range(len(lista)):
+    print("{} {}".format(indice, lista[indice]))
+
+# Compreensão de lista
+# código 
+
+
+lista.clear() # Limpa a lista
+print(lista)
