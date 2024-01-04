735a07767978b2b1c1dc6286321cc8c57b06f059
unknown <silvio.apad@gmail.com>
2024-01-03 21:12:34 -0400

    Desfazendo alterações com GIT

---
+++ aula04-Tuplas.py
@@ -0,0 +1,10 @@
+# Tuplas são imutáveis
+
+tupla = ("um", "dois", "tres", "quatro")
+tupla2 = ("é uma tupla",)
+tupla3 = ("não é uma tupla")
+print(tupla)
+print(type(tupla3))
+tupla4 = ("zero", 1, 2.0, True)
+print(tupla4)
+tupla5 = tuple("cinco", 5)
