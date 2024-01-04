3d068aaef595c1d3d013f00509ddea4264d8c195
unknown <silvio.apad@gmail.com>
2024-01-03 21:45:06 -0400

    Descompactando elementos de uma tupla

--- aula04-Tuplas.py
+++ aula04-Tuplas.py
@@ -1,3 +1,4 @@
+# TUPLAS
 # Tuplas são imutáveis
 
 tupla = ("um", "dois", "tres", "quatro")
@@ -7,4 +8,38 @@ print(tupla)
 print(type(tupla3))
 tupla4 = ("zero", 1, 2.0, True)
 print(tupla4)
-tupla5 = tuple("cinco", 5)
+tupla5 = tuple(("cinco", 6))
+
+# Acessando dados de uma tupla
+#   Mesma coisa das listas
+
+# Atualizando elementos de uma tupla
+#  Temos que converter para uma lista para atualizar elementos de uma tupla
+
+x = ("Gabriel", "Danny", "Arthur")
+print(x)
+
+y = list(x)
+y[1] = "Danielle"
+x = tuple(y)
+print(x)
+
+# y = list(x)
+# y.append("Lucas")
+# x = tuple(y)
+
+y = ("Lucas",)
+x += y
+print(x)
+
+y = list(x)
+y.remove("Arthur")
+x = tuple(y)
+print(x)
+
+del x
+
+# Descompactando tuplas
+
+(um, dois, *tres_e_quatro) = tupla
+print(type(um))
