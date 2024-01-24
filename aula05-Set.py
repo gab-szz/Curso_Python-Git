83a85aa7049cbae843305591774e880b20653819
unknown <silvio.apad@gmail.com>
2024-01-23 21:50:58 -0400

    Aprendendo a trabalhar com Sets em Python

--- aula05-Set.py
+++ aula05-Set.py
@@ -1,5 +1,18 @@
-# Uma coleção de itens não ordenada e indexada
+# Uma coleção de itens não ordenada e não indexada
 # Não permitem valores duplicados, ignorando um segundo valor igual e os elementos não podem ser alterados
 
+# Criando Sets
 set1 = {"um", "dois", "três"}
 set2 = {"abc", 2024, True}
+set3 = set(("cindo", "seis"))
+
+# Acessando item
+
+for i in set1:
+    print(i)
+
+# Adicionando elementos
+    
+set1.add("quatro")
+set1.update(set3)
+print(set1)
